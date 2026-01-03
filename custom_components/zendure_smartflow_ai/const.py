53a3429df711d38ed5777e1190df3584fa8f4259
PalmManiac <84145136+PalmManiac@users.noreply.github.com>
2026-01-03 11:10:52 +0100

    Bump integration version to 1.1.3

--- custom_components/zendure_smartflow_ai/const.py
+++ custom_components/zendure_smartflow_ai/const.py
@@ -10,7 +10,7 @@ DOMAIN = "zendure_smartflow_ai"
 INTEGRATION_NAME = "Zendure SmartFlow AI"
 INTEGRATION_MANUFACTURER = "PalmManiac"
 INTEGRATION_MODEL = "Home Assistant Integration"
-INTEGRATION_VERSION = "1.1.2"
+INTEGRATION_VERSION = "1.1.3"
 
 PLATFORMS: list[Platform] = [
     Platform.SENSOR,
